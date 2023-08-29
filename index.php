b78783de07540311dbbe4001ab2ac83794b5ee90
israel <zatur.pc@gmail.com>
2023-08-28 23:25:15 -0600

    agregados archivos -_-

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Israel Portillo ff48b963 - Sessions, Autos Database</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Bienvenido a la Base de Datos de Autos</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="view.php">View.php</a> without logging in - it should fail with an error message.
+<p>
+<p>
+Attempt to go to 
+<a href="add.php">add.php</a> without logging in - it should fail with an error message.
+<p>
+</div>
+</body>
+
